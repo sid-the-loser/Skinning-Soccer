76e036c950d50f69fadf8329d2a53162ccdcad01
Sidharth Suresh <sidharthsuresh07@gmail.com>
2024-10-23 00:19:42 -0400

    finished the main menu
    finished all four menus in the main menu scene
    
    ps. not all menus are complete, but they transition between each other

--- Assets/Sid/Scripts/Main Menu/MenuManager.cs
+++ Assets/Sid/Scripts/Main Menu/MenuManager.cs
@@ -1,4 +1,7 @@
+using System;
+using Sid.Scripts.Skin_Handler;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Sid.Scripts.Main_Menu
 {
@@ -6,10 +9,27 @@ namespace Sid.Scripts.Main_Menu
     {
         [SerializeField] private int currentMenuIndex;
         [SerializeField] private GameObject[] menuGameObjects;
+        [SerializeField] private Material[] materialsList;
 
+        [SerializeField] private GameObject ballGameObject;
+
+        private void OnEnable()
+        {
+            SkinHandler.MaterialsList = materialsList;
+        }
+
+        private void Start()
+        {
+            UpdateCurrentMenu();
+            UpdateBallMaterial();
+        }
+        
+        #region General
+        
         public void SetCurrentMenuIndex(int currentIndex)
         {
             currentMenuIndex = currentIndex;
+            UpdateCurrentMenu();
         }
 
         private void UpdateCurrentMenu()
@@ -23,5 +43,54 @@ namespace Sid.Scripts.Main_Menu
             menuGameObjects[currentMenuIndex].SetActive(true);
             
         }
+        
+        #endregion
+
+        #region Main Menu
+
+        public void QuitButtonLogic()
+        {
+            Application.Quit();
+        }
+
+        #endregion
+
+        #region Skin Select
+        
+        public void BallSkinNextButtonLogic()
+        {
+            SkinHandler.BallMaterialIndex += 1;
+            if (SkinHandler.BallMaterialIndex >= SkinHandler.MaterialsList.Length)
+            {
+                SkinHandler.BallMaterialIndex = 0;
+            }
+            UpdateBallMaterial();
+        }
+        
+        public void BallSkinPreviousButtonLogic()
+        {
+            SkinHandler.BallMaterialIndex -= 1;
+            if (SkinHandler.BallMaterialIndex < 0)
+            {
+                SkinHandler.BallMaterialIndex = SkinHandler.MaterialsList.Length - 1;
+            }
+            UpdateBallMaterial();
+        }
+
+        private void UpdateBallMaterial()
+        {
+            ballGameObject.GetComponent<Renderer>().material = SkinHandler.MaterialsList[SkinHandler.BallMaterialIndex];
+        }
+        
+        #endregion
+        
+        #region Timer Select
+
+        public void StartPlayingGame()
+        {
+            SceneManager.LoadScene("Test Scene");
+        }
+        
+        #endregion
     }
 }
---
+++ Assets/Sid/Scripts/Skin Handler/SkinHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Sid.Scripts.Skin_Handler
+{
+    public static class SkinHandler
+    {
+        public static Material[] MaterialsList;
+
+        public static int BallMaterialIndex;
+    }
+}
